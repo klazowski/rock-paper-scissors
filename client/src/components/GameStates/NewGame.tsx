ef832ee2c8875d1954b625428e14ceb4917e5a83
klazowski <klazowski+gitub@gmail.com>
2020-07-12 19:00:33 +0200

    Moved back to svg, but wrapped in img to enable scaling

--- client/src/components/GameStates/NewGame.tsx
+++ client/src/components/GameStates/NewGame.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import Triangle from '../images/Triangle';
+import BtnRock from '../GameComponents/BtnRock';
 
 const NewGame = (): JSX.Element => {
   return (
-    <div>
-      <Triangle />
+    <div className="new-game">
+      <img className="triangle" src="./images/bg-triangle.svg" width={200}></img>
+      <img className="paper" src="./images/icon-paper.svg" width={50}></img>
+      <img className="scissors" src="./images/icon-scissors.svg" width={50}></img>
+      <BtnRock />
     </div>
   );
 };
