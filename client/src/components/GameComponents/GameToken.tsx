e99357de1748ad45dd8e40a3185c77a49be0caad
klazowski <klazowski+gitub@gmail.com>
2020-07-27 20:54:41 +0200

    Moved token gradients inside token

--- client/src/components/GameComponents/GameToken.tsx
+++ client/src/components/GameComponents/GameToken.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
 
-const GameToken = (props: {
-  tokenSymbol: TokenSymbol;
-  tokenFrameColour: Gradient;
-}): JSX.Element => {
+const GameToken = (props: { tokenSymbol: TokenSymbol }): JSX.Element => {
   const areaSize = 150;
   const ringOuterDiameter: number = 130;
   const ringInnerDiameter: number = 100;
   const shadowOut: number = 5;
+
+  const gradients: { [type in string]: Gradient } = {
+    scissorsGradient: {
+      gradientStart: `hsl(40, 84%, 53%)`,
+      gradientEnd: `hsl(39, 89%, 49%)`,
+    },
+    paperGradient: {
+      gradientStart: `hsl(230, 89%, 65%)`,
+      gradientEnd: `hsl(230, 89%, 62%)`,
+    },
+    rockGradient: {
+      gradientStart: `hsl(349, 70%, 56%)`,
+      gradientEnd: `hsl(349, 71%, 52%)`,
+    },
+    lizardGradient: {
+      gradientStart: `hsl(261, 72%, 63%))`,
+      gradientEnd: `hsl(261, 73%, 60%)`,
+    },
+    spockGradient: {
+      gradientStart: `hsl(189, 58%, 57%)`,
+      gradientEnd: `hsl(189, 59%, 53%)`,
+    },
+  };
 
   return (
     <svg
@@ -20,11 +40,17 @@ const GameToken = (props: {
         <linearGradient id={`tokenGrad_${props.tokenSymbol}`} x1="0%" y1="0%" x2="0%" y2="100%">
           <stop
             offset="0%"
-            style={{ stopColor: props.tokenFrameColour.gradientStart, stopOpacity: 1 }}
+            style={{
+              stopColor: gradients[`${props.tokenSymbol}Gradient`].gradientStart,
+              stopOpacity: 1,
+            }}
           />
           <stop
             offset="100%"
-            style={{ stopColor: props.tokenFrameColour.gradientEnd, stopOpacity: 1 }}
+            style={{
+              stopColor: gradients[`${props.tokenSymbol}Gradient`].gradientEnd,
+              stopOpacity: 1,
+            }}
           />
         </linearGradient>
       </defs>
@@ -33,7 +59,7 @@ const GameToken = (props: {
         cy={areaSize / 2}
         r={ringInnerDiameter / 2}
         stroke={`url(#tokenGrad_${props.tokenSymbol})`}
-        stroke-width="15"
+        strokeWidth="15"
         fill="white"
       />
 
--- client/src/components/GameStates/NewGame.tsx
+++ client/src/components/GameStates/NewGame.tsx
@@ -6,26 +6,6 @@ const NewGame = (props: {
   gameType: GameType;
   onChoice: (event: React.MouseEvent, selectedToken: TokenSymbol) => void;
 }): JSX.Element => {
-  const scissorsGradient: Gradient = {
-    gradientStart: `hsl(40, 84%, 53%)`,
-    gradientEnd: `hsl(39, 89%, 49%)`,
-  };
-  const paperGradient: Gradient = {
-    gradientStart: `hsl(230, 89%, 65%)`,
-    gradientEnd: `hsl(230, 89%, 62%)`,
-  };
-  const rockGradient: Gradient = {
-    gradientStart: `hsl(349, 70%, 56%)`,
-    gradientEnd: `hsl(349, 71%, 52%)`,
-  };
-  const lizardGradient: Gradient = {
-    gradientStart: `hsl(261, 72%, 63%))`,
-    gradientEnd: `hsl(261, 73%, 60%)`,
-  };
-  const spockGradient: Gradient = {
-    gradientStart: `hsl(189, 58%, 57%)`,
-    gradientEnd: `hsl(189, 59%, 53%)`,
-  };
   return (
     <div className="new-game">
       <Triangle>
@@ -33,19 +13,19 @@ const NewGame = (props: {
           className={'paper'}
           onClick={(event: React.MouseEvent) => props.onChoice(event, 'paper')}
         >
-          <GameToken tokenSymbol="paper" tokenFrameColour={paperGradient} />
+          <GameToken tokenSymbol="paper" />
         </button>
         <button
           className={'rock'}
           onClick={(event: React.MouseEvent) => props.onChoice(event, 'rock')}
         >
-          <GameToken tokenSymbol="rock" tokenFrameColour={rockGradient} />
+          <GameToken tokenSymbol="rock" />
         </button>
         <button
           className={'scissors'}
           onClick={(event: React.MouseEvent) => props.onChoice(event, 'scissors')}
         >
-          <GameToken tokenSymbol="scissors" tokenFrameColour={scissorsGradient} />
+          <GameToken tokenSymbol="scissors" />
         </button>
       </Triangle>
     </div>
