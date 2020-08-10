a9d0222d3bce57f116a1829c38df712c74ab3451
klazowski <klazowski+gitub@gmail.com>
2020-08-10 17:22:11 +0200

    Added Battle component and updated token click handler

--- client/src/components/Game.tsx
+++ client/src/components/Game.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import NewGame from './GameStates/NewGame';
+import Battle from './GameComponents/Battle';
 
 const GameTokensSimple: TokenSymbol[] = ['rock', 'paper', 'scissors'];
 const GameTokensExtended: TokenSymbol[] = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
@@ -12,13 +13,32 @@ const Game = (): JSX.Element => {
 
   const tokenClickHandler = (event: React.MouseEvent, tokenSymbol: TokenSymbol) => {
     console.log(`Selected ${tokenSymbol}`);
+    setUserChoice(tokenSymbol);
+    setHouseChoice('paper');
     // Set next game state
+    setGameState('battle');
+  };
+
+  const playAgainClickHandler = (event: React.MouseEvent) => {
+    setUserChoice(undefined);
+    setHouseChoice(undefined);
+    // Set next game state
+    setGameState('new');
   };
 
   const gameBoard = () => {
     switch (gameState) {
       case 'new':
         return <NewGame gameType={'simple'} onChoice={tokenClickHandler} />;
+      case 'battle':
+        if (!userChoice || !houseChoice) return <p>Choice not accepted?</p>;
+        return (
+          <Battle
+            userChoice={userChoice}
+            houseChoice={houseChoice}
+            onPlayAgainClick={playAgainClickHandler}
+          />
+        );
       default:
         return <p>Error</p>;
     }
