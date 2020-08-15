86919c9ac9ecf7444d918957d4fd22343d4a5fcd
Krzysztof Lazowski <krzysztof.lazowski@electrolux.com>
2020-08-15 13:22:49 +0200

    Moved GameStates folder

--- client/src/components/Game.tsx
+++ client/src/components/Game.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import NewGame from './GameStates/NewGame';
+import NewGame from './GameComponents/GameStates/NewGame';
 import Battle from './GameComponents/Battle';
 import { calculateBattleResult } from '../engine/battle';
 
--- client/src/components/GameComponents/GameStates/NewGame.tsx
+++ client/src/components/GameComponents/GameStates/NewGame.tsx
@@ -1,31 +1,31 @@
 import React from 'react';
-import GameToken from '../GameComponents/GameToken';
-import Triangle from '../GameComponents/Triangle';
+import GameToken from '../GameToken';
+import Triangle from '../Triangle';
 
 const NewGame = (props: {
   gameType: GameType;
   onChoice: (event: React.MouseEvent, selectedToken: TokenSymbol) => void;
 }): JSX.Element => {
   return (
-    <div className="new-game">
+    <div className='new-game'>
       <Triangle>
         <button
           className={'paper'}
           onClick={(event: React.MouseEvent) => props.onChoice(event, 'paper')}
         >
-          <GameToken tokenSymbol="paper" />
+          <GameToken tokenSymbol='paper' />
         </button>
         <button
           className={'rock'}
           onClick={(event: React.MouseEvent) => props.onChoice(event, 'rock')}
         >
-          <GameToken tokenSymbol="rock" />
+          <GameToken tokenSymbol='rock' />
         </button>
         <button
           className={'scissors'}
           onClick={(event: React.MouseEvent) => props.onChoice(event, 'scissors')}
         >
-          <GameToken tokenSymbol="scissors" />
+          <GameToken tokenSymbol='scissors' />
         </button>
       </Triangle>
     </div>
